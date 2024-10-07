fc4000f2fbac747de3855f09d171d363b6258bb4
Refki Alfarizi <refkialfarizi123@gmail.com>
2024-10-07 16:03:58 +0700

    fix: removed temporary codes from initial commit

--- src/java/algeo01_23002/library/Matrix.java
+++ src/java/algeo01_23002/library/Matrix.java
@@ -5,38 +5,9 @@ import java.util.Arrays;
 public class Matrix {
   private double[][] data;
 
-  public Matrix(double[][] data) {
-    this.data = data;
-  }
 
-  public double[][] getData() {
-    return data;
-  }
 
-  public void setData(double[][] data) {
-    this.data = data;
-  }
 
-  public void println() {
-    for (int i = 0; i < data.length; i++) {
-      for (int j = 0; j < data[0].length; j++) {
-        System.out.print(data[i][j] + " ");
-      }
-      System.out.println();
-    }
-  }
-
-  public static Matrix add(Matrix a, Matrix b) {
-    double[][] dataA = a.getData();
-    double[][] dataB = b.getData();
-    double[][] dataC = new double[dataA.length][dataA[0].length];
-    for (int i = 0; i < dataA.length; i++) {
-      for (int j = 0; j < dataA[0].length; j++) {
-        dataC[i][j] = dataA[i][j] + dataB[i][j];
-      }
-    }
-    return new Matrix(dataC);
-  }
 
   @Override
   public boolean equals(Object obj) {
--- test/java/algeo01_23002/MainTest.java
+++ test/java/algeo01_23002/MainTest.java
@@ -6,23 +6,4 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 class MainTest {
-/*
-    @Test
-    void testMainOutput() {
-        // Capture the output from System.out
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream originalOut = System.out;
-        System.setOut(new PrintStream(outputStream));
-
-        // Call the main method
-        Main.main(new String[]{});
-
-        // Restore original System.out
-        System.setOut(originalOut);
-
-        // Check if the output matches "Hello, World!\n"
-        String output = outputStream.toString().trim();
-        assertEquals("Hello, World!", output);
-    }
-*/
 }

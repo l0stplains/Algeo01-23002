4921b3ca7049e86834061566d1596111f8c59c25
guntarahmbl <guntarahambali3@gmail.com>
2024-10-10 11:50:20 +0700

    feat: add Gauss & GaussJordan Solver

--- src/java/algeo01_23002/Main.java
+++ src/java/algeo01_23002/Main.java
@@ -1,8 +1,17 @@
 package algeo01_23002;
+import algeo01_23002.types.Matrix;
+import static algeo01_23002.solvers.LinearSystemSolver.Gauss;
+import static algeo01_23002.solvers.LinearSystemSolver.GaussJordan;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        Matrix matrix = new Matrix(4,6);
+        matrix.inputMatrix();
+
+        Matrix res = GaussJordan(matrix) ;
+        res.printMatrix();
     }
 
 }

03a4937a2510fd1ce0d6500a722ced5970648ffb
Refki Alfarizi <refkialfarizi123@gmail.com>
2024-10-24 23:53:15 +0700

    fix: interpolation Polynomial evaluate issue

--- src/java/algeo01_23002/types/PolynomialResult.java
+++ src/java/algeo01_23002/types/PolynomialResult.java
@@ -15,10 +15,10 @@ public class PolynomialResult {
     // Method to evaluate the polynomial at any value of x
     public double evaluate(double x) {
         double result = 0;
-        int degree = coefficients.getRowsCount() - 1;  // Assuming one-column matrix
+        int degree = coefficients.getColsCount() - 1;  // Assuming one-column matrix
 
         for (int i = 0; i <= degree; i++) {
-            result += coefficients.getData(i, 0) * Math.pow(x, i);
+            result += coefficients.getData(0, i) * Math.pow(x, i);
         }
 
         return result;
